712deabdddd9f77a75cb6b672bfe93faad82b8dd
Shahrukh489 <Shahrukh489@gmail.com>
2024-01-27 09:54:46 -0800

    implement mediator

---
+++ Infra.Bus/MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -0,0 +1,35 @@
+﻿namespace MicroRabbit.Infra.Bus;
+using System;
+using System.Threading.Tasks;
+using MediatR;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+
+public sealed class RabbitMQBus : IEventBus
+{
+    private readonly IMediator _mediator;
+    private readonly Dictionary<string, List<Type>> _handlers;
+    private readonly List<Type> _eventTypes;
+
+    public RabbitMQBus(IMediator mediator)
+    {
+        _mediator = mediator;
+        _handlers = new Dictionary<string, List<Type>>();
+        _eventTypes = new List<Type>();
+    }
+    public Task SendCommand<T>(T command) where T : Command
+    {
+        return _mediator.Send(command);
+    }
+    public void Publish<T>(T @event) where T : Event
+    {
+        throw new NotImplementedException();
+    }
+    public void Subscribe<T, TH>()
+        where T : Event
+        where TH : IEventHandler<T>
+    {
+        throw new NotImplementedException();
+    }
+}
